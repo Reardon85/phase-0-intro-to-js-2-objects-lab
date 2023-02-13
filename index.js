3b3ba377e5d4c75205793f282bfed8dca75ed80a
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-02-13 08:02:05 -0500

    commit tree 055b583f55abdb0c0604912e0af4b96cb5c9e54a

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    
+}
+
+function updateEmployeeWithKeyAndValue(emp, key1, value1){
+
+
+    const newEmp = {
+        ...emp, 
+    };
+    
+    newEmp[key1] = value1;
+    return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key1, value1){
+
+    emp[key1] = value1;
+    return emp;
+}
+
+function deleteFromEmployeeByKey(emp, key){
+    const newEmp = {...emp};
+    delete newEmp[key];
+    return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key){
+
+    delete emp[key];
+    return emp;
+}
